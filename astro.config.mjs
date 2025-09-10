84c15d8572e7c3d2c1a8acd30796ec789744e405
Rocío Pagliarulo <141795122+rpagliarulo@users.noreply.github.com>
2025-09-10 09:48:02 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rpagliarulo.github.io',
+  site: site: 'https://rpagliarulo.github.io/rpagliarulo.github.io',
   vite: {
       plugins: [tailwindcss()],
   },
