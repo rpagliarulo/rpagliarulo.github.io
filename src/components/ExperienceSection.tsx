133eba4cd2d55e86ef38f6df1c76d75d9eadf69a
rpagliarulo <rocioyasmin.pagliarulo@estudiantes.unahur.edu.ar>
2025-09-10 12:14:58 -0300

    Funciones ppales

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -46,7 +46,7 @@ export default function ExperienceSection() {
                   <div className="h-6 w-6 flex items-center justify-center rounded-full bg-purple-500/10 mr-2">
                     <Briefcase className="h-4 w-4 text-purple-500" />
                   </div>
-                  <h4 className="text-sm font-medium">Key Achievements</h4>
+                  <h4 className="text-sm font-medium">Funciones principales</h4>
                 </div>
                 <ul className="list-none ml-4 space-y-2 text-sm">
                   {job.achievements.map((achievement, i) => (
